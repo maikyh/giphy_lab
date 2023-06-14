9a8a5326e77bbe76333d2dcef942ed60dd1122fe
Miguel Garza Carranza <maiky@meta.com>
2023-06-14 12:44:13 -0700

    Implemented Giphy search fetch

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-console.log("Hello");
+console.log("Hello")
 
 var submitBtn = document.getElementById("submitBtn");
+var gifsDiv = document.getElementById("gifsDiv");
+var searchInput = document.getElementById("searchInput"); 
 
 submitBtn.addEventListener("click", async (event) => {
-    event.preventDefault();
+    event.preventDefault(); // Prevent form submission (optional)
+
+    // https://api.giphy.com/v1/gifs/search?api_key=PTLDGsZXmsUjYBOZIu74fBgVj3ytAkOo&q="hello"
 
     try {
         const apiKey = "llt6gQmigNxehMdFG50BTDZVcBNzv6Mq";
-        const searchTerm = "Hello";
-        const url = `https://api.giphy.com/v1/channels/search?api_key=${apiKey}&q=${encodeURIComponent(searchTerm)}`;
-        
+        const searchTerm = searchInput.value;
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(searchTerm)}`;
+    
         const response = await fetch(url);
         const data = await response.json();
+        const images = data.data;
+
+        images.forEach(gif => {
+            const img = document.createElement("img");
+            img.src = gif.images.original.url;
+            gifsDiv.appendChild(img); // Step 3: Add the image url to the gifsDiv
+        });
 
         console.log(data.data[0]);
-
-    }
-    catch (error) {
+        console.log(data.data[0].images.original.url);
+        // Process the data returned from the API
+        // console.log(data);
+    } catch (error) {
+        // Handle any errors that occur during the fetch request
         console.log(error);
     }
 
-    //https://api.giphy.com/v1/channels/search?api_key=llt6gQmigNxehMdFG50BTDZVcBNzv6Mq&q=Hello
-
-    console.log("Button Clicked!");
+    // Perform your desired action here
+    console.log("Button clicked!");
 });
-
